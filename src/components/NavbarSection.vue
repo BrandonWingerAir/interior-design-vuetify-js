bb4ae71adc1c52c1ab647b1efc556754fb5ba427
Brandon Winger-Air <brandonwinger-air@outlook.com>
2023-03-15 19:34:31 -0400

    navbar content & scroll method setup

--- src/components/NavbarSection.vue
+++ src/components/NavbarSection.vue
@@ -1,10 +1,26 @@
 <template>
-  <v-app-bar app color="black" dark flat class="px-12"></v-app-bar>
+  <v-app-bar app color="black" dark flat class="px-12">
+    <v-btn class="text-none">
+      <v-icon color="#8d6e63     " left class="mr-2 mb-1">fas fa-chair</v-icon>
+      inDesign
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn text @click="scroll('home')" color="#a1887f">Home</v-btn>
+    <v-btn text @click="scroll('portfolio')">Portfolio</v-btn>
+    <v-btn text @click="scroll('services')">Services</v-btn>
+    <v-btn text @click="scroll('about')">About</v-btn>
+    <v-btn text @click="scroll('contact')">Contact</v-btn>
+  </v-app-bar>
 </template>
 
 <script>
   export default {
-    
+    methods: {
+      scroll(refName) {
+        const element = document.getElementById(refName);
+        element.scrollIntoView({ behavior: "smooth" })
+      }
+    }
   }
 </script>
 
